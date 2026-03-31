c78a8f655e44627257070dcd955a901155ca52e9
sophia hardt <sophia_hardt@estudante.sesisenai.org.br>
2026-03-31 10:02:34 -0300

    add - comecei a lógica do java script

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+function soma() {
+    let {n1, n2} = guardar_valores();
+    (n1 + n2);
+}
+
+function subtracao() {
+    let {n1, n2} = guardar_valores();
+    (n1 - n2);
+}
+
+function multiplicacao() {
+    let {n1, n2} = guardar_valores();
+    (n1 * n2);
+}
+
+function divisao() {
+    let {n1, n2} = guardar_valores();
+    (n1 / n2);
+}
