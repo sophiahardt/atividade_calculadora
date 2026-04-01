c774c8160228422702bb1eca00c21b9516846c5b
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-04-01 11:46:45 -0300

    add - fiz algumas alterações

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,9 @@
+let visor;
+let valor1;
+let valor2;
+let operacao = "";
+
+
 function soma() {
     let {n1, n2} = guardar_valores();
     (n1 + n2);
@@ -17,3 +23,12 @@ function divisao() {
     let {n1, n2} = guardar_valores();
     (n1 / n2);
 }
+
+if operacao != "" {
+  visor = "5"
+}else {
+   let visor = document.getElementById("visor").value;
+   visor = visor + "1";
+}
+document.getElementById("visor").innerHTML = visor;
+
