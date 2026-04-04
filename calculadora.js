9ea8bf9f6d07afecc7c5dba5f88321a835628133
sophia hardt <sophia_hardt@estudante.sesisenai.org.br>
2026-04-03 21:05:05 -0300

    add - adicionei uma função para validar campos vazios

--- calculadora.js
+++ calculadora.js
@@ -3,14 +3,29 @@ let valor1;
 let valor2;
 let operacao = "";
 
+function validarCampos () {
+   let numero1 = document.getElementById("numero1").value.trim();
+   if (numero1 === "") {
+      alert("Número 1 está vazio");
+      return false;
+   } 
+   let numero2 = document.getElementById("numero2").value.trim();
+   if (numero2 === "") {
+      alert("Número 2 está vazio");
+      return false;
+   } 
+   return true;
+}
 
 function soma() {
-   console.log("Valor 1: [" + document.getElementById("numero1").value + "]");
-   console.log("Valor 2: [" + document.getElementById("numero2").value + "]");
-   let valor1 = parseInt(document.getElementById("numero1").value) + parseFloat(document.getElementById("numero2").value);
-   console.log("Resultado: [" + valor1.toString() + "]");
-   alert("Resultado: " + valor1.toString());
-   document.getElementById("resultado").innerText = valor1.toString();
+   if (validarCampos()) {
+      console.log("Valor 1: [" + document.getElementById("numero1").value + "]");
+      console.log("Valor 2: [" + document.getElementById("numero2").value + "]");
+      let valor1 = parseInt(document.getElementById("numero1").value) + parseFloat(document.getElementById("numero2").value);
+      console.log("Resultado: [" + valor1.toString() + "]");
+      alert("Resultado: " + valor1.toString());
+      document.getElementById("resultado").innerText = valor1.toString();
+   }
 }
 
 function subtracao() {
